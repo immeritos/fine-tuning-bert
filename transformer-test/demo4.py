27a7ed71ef3be041248a0cc2de37f65f63d6bd8d
immeritos <immeritos@gmail.com>
2025-09-22 19:04:56 +0200

    update

--- transformer-test/demo4.py
+++ transformer-test/demo4.py
@@ -1,11 +1,7 @@
 from datasets import load_dataset
 
-ds = load_dataset("ndiy/ChnSentiCorp")
+ds = load_dataset("lansinuote/ChnSentiCorp")
 
 ds.save_to_disk("./data/ChnSentiCorp_arrow")
 
-import os
-os.makedirs("./data/ChnSentiCorp_csv", exist_ok=True)
-for split in ds.keys():  # 例如: 'train', 'validation', 'test'
-    ds[split].to_csv(f"./data/ChnSentiCorp_csv/{split}.csv", index=False)
 print(ds)
